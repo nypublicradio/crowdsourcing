1ead1dba1e0ef4b400d7557baddf528e37fafbda
Brian Whitton <me@brianwhitton.com>
2017-10-06 17:20:20 -0400

    adds validate and create methods

--- surveys/serializers.py
+++ surveys/serializers.py
@@ -21,3 +21,22 @@ class SubmissionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Submission
         fields = ('id', 'survey', 'submitted_at', 'answers')
+
+    def validate(self, data):
+        allowed_questions = set([q.pk for q in data['survey'].questions.all()])
+        answered_questions = set([a['question'] for a in data['answers']])
+        if not allowed_questions.issuperset(answered_questions):
+            # only accept answers for the given survey
+            raise serializers.ValidationError(
+                'Submission includes answers to questions that do not belong to this survey.'
+            )
+        return data
+
+    def create(self, validated_data):
+        questions = validated_data['survey'].questions.values()
+        # add metadata from the question to answers in the JSON blob
+        for answer in validated_data['answers']:
+            question = questions.get(pk=answer['question'])
+            answer['label'] = question['label']
+            answer['input_type'] = question['input_type']
+        return validated_data
