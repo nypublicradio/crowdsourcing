5784becb8c0182f0a55e42b2c2dd30e257a22181
Brian Whitton <me@brianwhitton.com>
2017-10-12 13:53:17 -0400

    validation test

--- surveys/tests.py
+++ surveys/tests.py
@@ -1,3 +1,5 @@
+from unittest.mock import Mock, patch
+
 from django.urls import reverse
 
 from mixer.backend.django import mixer
@@ -42,3 +44,32 @@ class SubmissionTests(APITestCase):
             self.assertEqual(expected_keys, sorted(answer.keys()))
             self.assertEqual(answer['input_type'], question.input_type)
             self.assertEqual(answer['label'], question.label)
+
+    @patch('surveys.validators.requests.head')
+    def test_validations(self, mock_head):
+        url = reverse('submission-list')
+        survey = mixer.blend(Survey,)
+        email_question = mixer.blend(Question, input_type=Question.EMAIL,
+                                     label='email', survey=survey)
+        audio_question = mixer.blend(Question, input_type=Question.AUDIO,
+                                     label='audio', survey=survey)
+        bad_email = 'bademail@'
+        bad_audio = 'http://not-a-file.mp3'
+        data = {
+            'survey': survey.pk,
+            'answers': [{
+                'question': email_question.pk,
+                'response': bad_email,
+            }, {
+                'question': audio_question.pk,
+                'response': bad_audio
+            }]
+        }
+        mock_head.return_value = Mock(status_code=404)
+        response = self.client.post(url, data, format='json')
+        errors = response.data
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        mock_head.assert_called_with(bad_audio)
+        self.assertEqual(errors['audio'], ['Audio file {} does not exist.'.format(bad_audio)])
+        self.assertEqual(errors['email'], ['Enter a valid email address.'])
