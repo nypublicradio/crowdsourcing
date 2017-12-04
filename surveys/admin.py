a4186b8c005e9fdd23a71a48775948029f38236d
Brian Whitton <me@brianwhitton.com>
2017-12-04 18:20:39 -0500

    updates admin list view for submissions
    adds audio player to playback submitted audio
    links out to related survey
    put submission timestamp in its own column for sorting

--- surveys/admin.py
+++ surveys/admin.py
@@ -1,6 +1,8 @@
 from adminsortable2.admin import SortableInlineAdminMixin
 
 from django.contrib import admin
+from django.utils.html import format_html_join, format_html
+from django.core.urlresolvers import reverse
 
 from .models import Survey, Submission, Question
 
@@ -24,6 +26,19 @@ class SurveyAdmin(admin.ModelAdmin):
 
 class SubmissionAdmin(admin.ModelAdmin):
     list_filter = ['survey']
+    list_display = ('__str__', 'submitted_at', 'audio_files', 'survey_link')
+
+    def audio_files(self, obj):
+        return format_html_join(
+            '\n',
+            '<audio src="{}" controls />',
+            ([file] for file in obj.audio_answers)) if obj.audio_answers else None
+    audio_files.short_description = 'Audio Answers'
+
+    def survey_link(self, obj):
+        survey = obj.survey
+        return format_html('<a href="{}">{}</a>',
+                           reverse('admin:surveys_survey_change', args=(survey.pk,)), survey)
 
 
 admin.site.register(Survey, SurveyAdmin)
