5d32c94ba3419af81c2cd5dff45878a5e1424aa1
Brian Whitton <me@brianwhitton.com>
2018-01-16 17:39:32 -0500

    make this its own migration

--- surveys/migrations/0004_expiration_message.py
+++ surveys/migrations/0004_expiration_message.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('surveys', '0002_field_lengths_type_and_default'),
+        ('surveys', '0003_expiration_dates'),
     ]
 
     operations = [
         migrations.AddField(
             model_name='survey',
-            name='ends_at',
-            field=models.DateTimeField(blank=True, null=True, verbose_name='Ends At'),
+            name='expired_message',
+            field=models.TextField(blank=True, help_text='\n    Message to display to users after this survey has expired.\n    ', verbose_name='Expiration Message'),
         ),
     ]
