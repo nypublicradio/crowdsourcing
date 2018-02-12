1a8896b8f91ad39afb5b4aaabcf209b7e8287f30
Bryn Scharenberg <bscharenberg@nypublicradio.org>
2018-02-12 10:52:22 -0500

    Commit to add action to submissions view that will allow export of csv of submissions data.

--- surveys/models.py
+++ surveys/models.py
@@ -70,3 +70,19 @@ class Submission(models.Model):
     @property
     def audio_answers(self):
         return [x for x in filter(lambda x: x['input_type'] == 'a', self.answers)]
+
+    @property
+    def labels(self):
+        return [x['label'] for x in self.answers]
+
+    @property
+    def responses(self):
+        return [x.get('response', '') for x in self.answers]
+
+    @property
+    def surveyid(self):
+        return self.survey.id
+
+    @property
+    def surveytitle(self):
+        return self.survey.title
